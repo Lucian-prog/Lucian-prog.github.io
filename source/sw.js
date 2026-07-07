56a92ee5c9ab6b09d1f6c7064fdcc2a2c06a9917
LZX <2512640924@qq.com>
2026-07-07 16:40:38 +0800

    Clear legacy PWA cache

---
+++ source/sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    if (self.caches) {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((key) => caches.delete(key)));
+    }
+
+    await self.clients.claim();
+
+    const clients = await self.clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    });
+
+    await self.registration.unregister();
+
+    for (const client of clients) {
+      client.navigate(client.url);
+    }
+  })());
+});
+
+self.addEventListener('fetch', () => {});
